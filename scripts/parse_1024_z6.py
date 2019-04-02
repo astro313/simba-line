f2da0f7b22a9047f456550a5f61fbda2494a0089
Daisy Leung <leungtszkuk@gmail.com>
2019-04-02 02:55:19 +0000

    speed up

--- scripts/parse_1024_z6.py
+++ scripts/parse_1024_z6.py
@@ -5,7 +5,11 @@ getting all z~6 galaxies in the 1024 sims, from the 25 cMpc/h box
 
 """
 
-from parse import *
+import cPickle as pickle
+try:
+    from parse import *
+except:
+    from parse_simba import *
 
 raw_sim_dir = '/disk01/rad/sim/m25n1024/s50/'
 raw_sim_name_prefix = 'snap_m25n1024_'
@@ -18,17 +22,28 @@ zCloudy = 6
 d_data = '/home/dleung/Downloads/SIGAME_dev/sigame/temp/z' + str(int(zCloudy)) + '_data_files/'
 
 # this doesn't affect ouput, just for inspection
-Nhalo = 10
-Ngalaxies = 10
-total_Ngal = get_basic_info_from_caesarCat(snapRange, Nhalo, Ngalaxies, caesar_dir, name_prefix)
+# Nhalo = 10
+# Ngalaxies = 10
+# total_Ngal = get_basic_info_from_caesarCat(snapRange, Nhalo, Ngalaxies, caesar_dir, name_prefix)
 
-print "Using all galaxies found from the Caesar catalog"
-print total_Ngal
-Ngalaxies = total_Ngal
+# print "Using all galaxies found from the Caesar catalog"
+# print total_Ngal
+# Ngalaxies = total_Ngal
 
-ggg = select_SFgal_from_simba(raw_sim_dir, raw_sim_name_prefix, caesar_dir, name_prefix, snapRange, Ngalaxies)
+# ggg = select_SFgal_from_simba(raw_sim_dir, raw_sim_name_prefix, caesar_dir, name_prefix, snapRange, Ngalaxies)
 
-xx, yy = simba_to_pd(ggg, raw_sim_dir, raw_sim_name_prefix, caesar_dir, name_prefix, redshiftFile, d_data, zCloudy, plotgas=False)
+# # for debugging, I don't want to have to run select_SFgal_from_simba() again and again
+# with open('ggg.pkl', 'w') as f:
+#     pickle.dump([ggg], f)
+
+with open('ggg.pkl', 'r') as f:
+    ggg = pickle.load(f)
+
+
+galnames, zred = simba_to_pd(ggg, raw_sim_dir, raw_sim_name_prefix, caesar_dir, name_prefix, redshiftFile, d_data, zCloudy, plotgas=False)
+
+_, _ = pd_bookkeeping(galnames, zred)
+
 
 fetch_BH(ggg, raw_sim_dir, raw_sim_name_prefix, caesar_dir, name_prefix, redshiftFile)
 
