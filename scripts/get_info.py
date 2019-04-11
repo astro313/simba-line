9371db20569f8a8aa193ba9139484ace71d6c6ff
Daisy Leung <leungtszkuk@gmail.com>
2019-04-11 16:57:33 -0400

    proper rendering of textfile

--- scripts/get_info.py
+++ scripts/get_info.py
@@ -6,7 +6,7 @@ import os
 import sys
 from readgadget import readsnap
 
-def info(obj, snapFile, top, savetxt=False):
+def info(obj, snapFile, top=None, savetxt=False):
     """
     Customized version to print more info on the CAESAR extracted galaxies.
 
@@ -22,7 +22,7 @@ def info(obj, snapFile, top, savetxt=False):
     group_list = obj.galaxies
     nobjs = len(group_list)
 
-    if top > nobjs:
+    if top > nobjs or top is None:
         top = nobjs
 
     if obj.simulation.cosmological_simulation:
@@ -38,7 +38,7 @@ def info(obj, snapFile, top, savetxt=False):
 
     cnt = 1
 
-    output += ' ID      Mstar     Mgas      MBH    MBH_dot [Msun/yr]    SFR [Msun/yr]       r_baryon   r_gas      r_gas_half_mass      r_stellar    r_stellar_half_mass    Z_sfrWeighted [/Zsun]    Z_massWeighted     Z_stellar     T_gas_massWeighted    T_gas_SFRWeighted   fgas   nrho      Central\t|  Mhalo     HID\n'
+    output += ' ID      Mstar     Mgas      MBH    fedd    SFR [Msun/yr]       r_baryon   r_gas      r_gas_half_mass      r_stellar    r_stellar_half_mass    Z_sfrWeighted [/Zsun]    Z_massWeighted     Z_stellar     T_gas_massWeighted    T_gas_SFRWeighted   fgas   nrho      Central\t|  Mhalo     HID\n'
     output += ' ----------------------------------------------------------------------------------------\n'
 
     h = obj.simulation.hubble_constant
@@ -50,20 +50,23 @@ def info(obj, snapFile, top, savetxt=False):
         if o.halo is not None:
             phm, phid = o.halo.masses['total'], o.halo.GroupID
 
-        print o.masses['bh']
-        import pdb; pdb.set_trace()
+        try:
+            bhmdots = [bhmdot[k] for k in o.bhlist]
+            imax = np.argmax(o.masses['bh'])
+            bm = o.masses['bh'][imax]
 
-        bhmdots = [bhmdot[k] for k in o.bhlist]
-        imax = np.argmax(o.masses['bh'])
-        bmdot = bhmdots[imax]        # only the massive BH particle matters.
-
-        print o.masses['bh'], bmdot
-        import pdb; pdb.set_trace()
+            frad = 0.1
+            mdot_edd = 4*np.pi*6.67e-8*1.673e-24/(frad*3.e10*6.65245e-25) * bm * 3.155e7 # in Mo/yr
+            bmdot = bhmdots[imax]        # only the massive BH particle matters.
+            fedd = bmdot / mdot_edd
+        except:
+            bm = 0
+            fedd = 0
 
         output += ' %04d  %0.2e  %0.2e  %0.2e  %0.2e  %0.2e  %0.2e  %0.2e  %0.2e  %0.2e  %0.2e   %0.3f   %0.3f  %0.3f  %0.2e  %0.2e  %0.3f  %0.2e  %s\t|  %0.2e  %d \n' % \
                   (o.GroupID, o.masses['stellar'], o.masses['gas'],
-                   o.masses['bh'],
-                   bmdot,
+                   bm,
+                   fedd,
                    o.sfr,
                    o.radii['baryon'],
                    o.radii['gas'],
@@ -83,8 +86,76 @@ def info(obj, snapFile, top, savetxt=False):
         if cnt > top: break
 
     print(output)
-    # if savetxt:
+
+    if savetxt:
+        outputFileName = snapFile[50:-5] + '_top' + str(top) + '.txt'
+        f = open(outputFileName, 'w')    # 'a'
+        f.write(output)
+        f.close()
+
     return output
+
+
+def setup_cmap(cm='magma'):    # "plasma_r"
+    import matplotlib.pyplot as plt
+    return plt.set_cmap(cm)
+
+
+def setup_plot():
+    import matplotlib
+    from cycler import cycler
+    # print(matplotlib.matplotlib_fname())
+
+    matplotlib.rcParams.update({'figure.figsize': (8, 5)    # inches
+                                , 'font.size': 16      # points
+                                , 'legend.fontsize': 10      # points
+                                , 'lines.linewidth': 2       # points
+                                , 'axes.linewidth': 1       # points
+                                , 'axes.prop_cycle': cycler('color', 'bgrcmyk')
+                                , 'text.usetex': True
+                                , 'font.family': "serif"  # Use serifed fonts
+                                , 'xtick.major.size': 13     # length, points
+                                , 'xtick.major.width': 1     # points
+                                , 'xtick.minor.size': 8     # length, points
+                                , 'xtick.minor.width': 1     # points
+                                , 'ytick.major.size': 13     # length, points
+                                , 'ytick.major.width': 1     # points
+                                , 'ytick.minor.size': 8     # length, points
+                                , 'xtick.labelsize': 14
+                                , 'ytick.labelsize': 14
+                                , 'ytick.minor.width': 1     # points
+                                , 'font.serif': ("times", "Computer Modern Roman", "New Century Schoolbook", "Bookman"), 'font.sans-serif': ("Helvetica", "Avant Garde", "Computer Modern Sans serif"), 'font.monospace': ("Courier", "Computer Modern Typewriter"), 'font.cursive': "Zapf Chancery"
+                                })
+    cm = setup_cmap()
+    return cm
+
+
+def plot_info(colNumx, colNumy, inFile, logx=True, logy=True, xlabel='',
+              ylabel='',
+              tag='',
+              saveFig=True):
+    """
+
+    colNumx: int
+        column to read from the .txt file, used as x-axis in plotting
+
+    colNumy: int
+        column to read from the .txt file, used as x-axis in plotting
+
+    inFile: str
+        name of input file to read from where we extract global properties of galaxies
+
+
+    """
+
+    import matplotlib.pyplot as plt
+
+    cm = setup_plot()
+
+    plt.close('all')
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
 
 
 
@@ -114,8 +185,8 @@ if __name__ == '__main__':
 
     infile = caesar_dir + name_prefix + '{:0>3}'.format(int(36)) + \
                 '.hdf5'
-    obj = caesar.load(infile)
+    obj = caesar.load(infile, LoadHalo=False)
     snapFile = raw_sim_dir + raw_sim_name_prefix + '{:0>3}'.format(int(36)) + '.hdf5'
 
-    output = info(obj, snapFile, top=1)
+    output = info(obj, snapFile, top=None, savetxt=True)
 
