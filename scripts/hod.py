35a66c3fa925457f347c0879278c19f603e6c1f1
Daisy Leung <leungtszkuk@gmail.com>
2019-09-09 11:49:38 -0400

    working

---
+++ scripts/hod.py
@@ -0,0 +1,152 @@
+
+import caesar
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib
+# Specify renderer
+# matplotlib.use('Agg')
+# Boiler-plate settings for producing pub-quality figures
+# 1 point = 1/72 inch
+matplotlib.rcParams.update({'figure.figsize'  : (8,5)    # inches
+                            ,'font.size'       : 22      # points
+                            ,'legend.fontsize' : 16      # points
+                            ,'lines.linewidth' : 2       # points
+                            ,'axes.linewidth'  : 2       # points
+                            ,'text.usetex'     : True    # Use LaTeX to layout text
+                            ,'font.family'     : "serif" # Use serifed fonts
+                            ,'xtick.major.size'  : 6     # length, points
+                            ,'xtick.major.width' : 2     # points
+                            ,'xtick.minor.size'  : 3     # length, points
+                            ,'xtick.minor.width' : 1     # points
+                            ,'ytick.major.size'  : 6     # length, points
+                            ,'ytick.major.width' : 2     # points
+                            ,'ytick.minor.size'  : 3     # length, points
+                            ,'ytick.minor.width' : 1     # points
+                            ,'font.serif'      : ("Times"
+                                                  ,"Palatino"
+                                                  ,"Computer Modern Roman"
+                                                  ,"New Century Schoolbook"
+                                                  ,"Bookman")
+                            ,'font.sans-serif' : ("Helvetica"
+                                                  ,"Avant Garde"
+                                                  ,"Computer Modern Sans serif")
+                            ,'font.monospace'  : ("Courier"
+                                                  ,"Computer Modern Typewriter")
+                            ,'font.cursive'    : "Zapf Chancery"
+})
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.special import erf
+import h5py
+import sys
+from astropy.cosmology import FlatLambdaCDM
+from scipy import stats
+
+
+infile = '/mnt/ceph/users/daisyleung/simba/sim/m50n512/s50/Groups/m50n512_036.hdf5'
+# infile = '/mnt/ceph/users/daisyleung/simba/sim/m100n1024/s50/Groups/m100n1024_036.hdf5'
+# infile = '/mnt/ceph/users/daisyleung/simba/sim/m25n256/s50_new/Groups/m25n256_036.hdf5'
+
+#
+# infile = '/mnt/ceph/users/daisyleung/simba/sim/m50n1024/s50/Groups/m50n1024_036.hdf5'
+# infile = '/mnt/ceph/users/daisyleung/simba/sim/m25n1024/s50_new/Groups/m25n1024_036.hdf5'
+
+nbin = 20
+
+sim = caesar.load(infile) # load caesar file
+mlim = 64*sim.simulation.critical_density.value*sim.simulation.boxsize.value**3*sim.simulation.omega_matter/sim.simulation.effective_resolution**3 # galaxy mass resolution limit in DM particle
+
+vol_mpc = sim.simulation.boxsize.to('Mpccm').d**3
+cosmo = FlatLambdaCDM(H0=100*sim.simulation.hubble_constant, Om0=sim.simulation.omega_matter, Ob0=sim.simulation.omega_baryon,Tcmb0=2.73)
+
+myobjs = sim.galaxies
+cents = np.asarray([i for i in myobjs if i.central==1])
+Mhmin = np.min( np.asarray([i.halo.masses['total'] for i in cents]) ) # smallest halo to host a galaxy in each cental halo
+halos = np.asarray([i for i in sim.halos if i.masses['total']>Mhmin])
+mh = np.asarray([i.masses['total'] for i in halos])
+
+pos = np.asarray([i.pos for i in halos])
+print('Mhalo,min=',np.log10(Mhmin),' Ncents=',len(cents),' Nhalos=',len(halos))
+# Mhalo,min= 9.494088135336268  Ncents= 9924  Nhalos= 64988
+# Mhalo,min= 10.45835996211413  Ncents= 3825  Nhalos= 24193
+
+ms = np.asarray([i.masses['stellar'] for i in myobjs])
+logms = np.log10(ms)
+sfr = np.asarray([i.sfr for i in myobjs])
+ssfr = np.log10(1.e9*sfr/ms+10**(-2.9+0.3*sim.simulation.redshift)) # with a floor to prevent NaN's
+
+halogals = np.asarray([i.galaxy_index_list for i in halos])
+halomags = []
+for i in range(len(halogals)):
+    halomags.append(halogals[i])
+ngal = np.asarray([len(hm) for hm in halomags])
+
+
+# plot
+xp = np.log10(mh)
+yp = np.log10(ngal)
+posp = pos
+
+bins = 8
+boxsize = sim.simulation.boxsize
+print(boxsize, vol_mpc)
+
+
+# # ---- added ----
+# bin_edges = np.arange(0.999 * min(xp), 1.001 * max(xp), (max(xp) - min(xp)) / (bins))
+# bin_means, bin_edges, _ = stats.binned_statistic(xp, yp, bins=bins, statistic='mean')
+# bin_cent = 0.5 * (bin_edges[1:] + bin_edges[:-1])
+# print(bin_means)
+# # if np.isinf(np.abs(bin_means)).any():
+# #    import pdb; pdb.set_trace()
+
+# plt.figure()
+# plt.plot(bin_cent, bin_means, '-', linewidth=2, color='k', label=None)
+
+
+# # put galaxies into octants
+# pos = np.floor(posp / (0.5 * boxsize)).astype(np.int)
+# gal_index = pos[:, 0] + pos[:, 1] * 2 + pos[:, 2] * 4
+
+# bin_oct = np.zeros((abs(bins), 8))
+# for i0 in range(8):
+#     mh = xp[gal_index == i0]
+#     ngal = yp[gal_index == i0]
+#     bin_oct[:, i0], bin_edges, binnumber = stats.binned_statistic(mh, ngal, bins=bin_edges, statistic='mean')
+# bin_oct[:, i0].shape
+# bin_oct = np.ma.masked_invalid(bin_oct)
+# var = np.ma.std(bin_oct, axis=1)
+# print(var)
+# plt.errorbar(bin_cent, bin_means, yerr=[
+#             var, var], fmt='o', linewidth=2, color='k')
+# plt.savefig('test.pdf')
+
+# import function as fu
+# import pdb; pdb.set_trace()
+# x, y, var = fu.cosmic_variance(mh, pos, boxsize, boxsize, nbin=8, minmass=-1)
+# print(var)
+# # ---------------
+
+
+plt.close('all')
+plt.figure()
+import plotmedian as pm
+# I just estimate it based on the spread on individual quantities over 8 octants.  8 is purely for convenience, and seemed like a sufficiently large number to get a decent variance.  I’ve never compared to calculating it from the correlation function, which would probably be more accurate.
+pm.plotmedian(np.log10(mh),np.log10(ngal),ltype='-',lw=2,
+                ax=plt,
+                bins=nbin,
+                stat='mean',
+                pos=pos,
+                boxsize=sim.simulation.boxsize,
+                label=r'$z$ = %g'%(np.round(sim.simulation.redshift,1))
+              )
+
+plt.minorticks_on()
+# plt.xlim(11.0,)
+plt.ylabel(r'$<$log N$>$',fontsize=16)
+plt.xlabel(r'log $M_{\rm halo}$ [$M_{\odot}$]' ,fontsize=16)
+plt.legend(loc='upper left')
+
+plt.savefig('m50n512_hod.pdf', bbox_inches='tight', format='pdf')
+plt.show()
